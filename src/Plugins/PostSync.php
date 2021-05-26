e83dcbd4210b5e2ab8f878f89c6411da94b4df57
Md. Anam Hossain <anam@themexpert.com>
2021-05-26 16:56:50 +0600

    open rest route for connected app

--- src/Plugins/PostSync.php
+++ src/Plugins/PostSync.php
@@ -124,4 +124,40 @@ final class PostSync extends Plugin
         return $post_types;
     }
     
+    /**
+     * @since 0.6.2
+     * @param string $query_string for search among all post type
+     * @access public 
+     * @return JSON
+     */
+    public function get_post_search_result($query_string=''){
+        $x_query = new \WP_Query( 
+            array( 
+                's' => $query_string,
+                'post_type' => get_option('thrivedesk_post_type_sync_option')
+            ) 
+        );
+        $search_posts = [];
+        while($x_query->have_posts()):
+            $x_query->the_post();
+            $search_posts[get_the_ID()] = [
+                'id'    => get_the_ID(),
+                'title' => get_the_title(),
+                'link'  => get_the_permalink(),
+                'excerpt' => get_the_excerpt(),
+            ];
+            
+        endwhile;
+        if(empty($search_posts)){
+            return [
+                'data' => 'nothing found'
+            ];
+        }else{
+            return [
+                'count' => count($search_posts). ' result found',
+                'data'  =>  $search_posts 
+            ];
+        }
+        wp_reset_query();
+    }
 }
--- src/Restroute.php
+++ src/Restroute.php
@@ -1,32 +1,59 @@
 <?php 
 namespace ThriveDesk;
+use \ThriveDesk\Plugins\PostSync;
 
 if(!defined('ABSPATH')){
     exit;
 }
 class Restroute {
+    /**
+     * The single instance of this class
+     */
     private static $instance;
-
+    /**
+     * The single instance of postsync
+     */
+    private $postsync;
+    /**
+     * Main RestRoute Instance.
+     * @return PostSync|null
+     * @access public
+     * @since 0.7.0
+     */
     public static function instance(){
         if(null === self::$instance ){
             self::$instance = new self;
         }
         return self::$instance;
     }
+    
     private function __construct(){
-
-        add_action( 'rest_api_init', array($this, 'td_routes') );
+        $this->postsync = PostSync::instance();
+        if($this->postsync->get_plugin_data('connected')):
+            add_action( 'rest_api_init', array($this, 'post_query_routes') );
+        endif;
     }
-    public function td_routes(){
+    /**
+     * @since 0.6.2
+     * @access public 
+     * @return void
+     */
+    public function post_query_routes(){
         register_rest_route('thrivedesk', '/search', array(
             'methods' => 'get',
-            'callback' => array($this, 'td_pull_site_search_data')
+            'callback' => array($this, 'get_post_search_data')
         ));
     }
-    public function td_pull_site_search_data($request){
+    /**
+     * @since 0.6.2
+     * @param string $request for search among all post type
+     * @access public 
+     * @return JSON
+     */
+    public function get_post_search_data($request){
         $query_string_param = $request->get_params('query');
         
-        $search_result = td_get_search_result($query_string_param['query']);
+        $search_result = $this->postsync->get_post_search_result($query_string_param['query']);
         
         $site_data = [
             'search_result' => $search_result,
