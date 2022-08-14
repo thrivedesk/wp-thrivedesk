15287523a19d06a190cc8231b5be7d8acc714798
Md Atiqur Rahman <57439878+atiq-ur@users.noreply.github.com>
2022-08-14 17:43:46 +0600

    add td settings option

--- resources/js/admin.js
+++ resources/js/admin.js
@@ -1,3 +1,5 @@
+import Swal from "sweetalert2";
+
 jQuery(document).ready(($) => {
 	$('.thrivedesk button.connect').on('click', function (e) {
 		e.preventDefault();
@@ -79,19 +81,53 @@ jQuery(document).ready(($) => {
 	// settings tab
 	$( "#form_provider" ).change(function() {
 		let selected_item_id = $("#form_provider").val();
+		let form = {
+			'value': selected_item_id,
+		}
 		$.ajax({
-			type: "GET",
-			url: "/wp-json/td-settings/forms/"+selected_item_id,
+			type: "POST",
+			url: "/wp-json/td-settings/forms",
+			data: form,
 			success: function(data){
-				console.log(data)
+				// console.log(data)
 				let option = "";
 				option +='<option value="">Please choose a form</option>';
 				data.forEach(function(element) {
-					option  += "<option value = '"+ element.id + "'>"+ element.title +"</option>";
+					option  += "<option value = '"+ element.key + "'>"+ element.title +"</option>";
 				});
 				$("#form_name").html(option);
 			}
 		});
+	});
+
+	$('#td_setting_btn_submit').click(function(e){
+		e.preventDefault();
+		let form_provider = $("#form_provider").val();
+		let form_name = $("#form_name").val();
+
+		$.ajax({
+			type: "POST",
+			url: "/wp-json/td-settings/form/submit",
+			data: {
+				form_provider: form_provider,
+				form_name: form_name,
+			},
+			success: function(data){
+				Swal.fire({
+					title: 'Great',
+					icon: 'success',
+					text: 'Setting saved successfully',
+					showClass: {
+						popup: 'animate__animated animate__fadeInDown'
+					},
+					hideClass: {
+						popup: 'animate__animated animate__fadeOutUp'
+					},
+					timer: 4000
+				});
+			}
+		});
+
 	});
 
 	// $( "#form_name" ).change(function() {
