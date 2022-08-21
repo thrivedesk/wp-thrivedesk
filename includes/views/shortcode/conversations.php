cffc2add23047d322b2ba4aa73988a71e564d145
atiq-ur <ar.diu.cse@gmail.com>
2022-08-21 17:46:01 +0600

    feat(conversation): add search layout on search modal

--- includes/views/shortcode/conversations.php
+++ includes/views/shortcode/conversations.php
@@ -57,7 +57,8 @@
 				<td class="py-4 px-6 text-right">
 <!--					<a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>-->
                     <br>
-					<a href="<?php echo home_url(). '/conversation_details_page/action=details_page'; ?>" class="font-medium
+					<a href="<?php echo home_url(). '/td-shortcode?conversation_id=12'; ?>"
+                       class="font-medium
 					text-blue-600
 					dark:text-blue-500
 					hover:underline">View
@@ -151,7 +152,8 @@
 </div>
 
 <!-- Main modal -->
-<div id="tdConversationModal" style="display: none" tabindex="-1" class="td-modal overflow-y-auto overflow-x-hidden hidden
+<div id="tdConversationModal" style="display: none" tabindex="-1"
+     class="td-modal overflow-y-auto overflow-x-hidden hidden
 fixed top-0
 right-0
 left-0 z-50
@@ -172,42 +174,149 @@ w-full md:inset-0 h-modal md:h-full justify-center items-center flex" aria-modal
                 </button>
             </div>
             <!-- Modal body -->
-            <div class="p-6 space-y-6">
-                <form class="flex items-center">
-                    <label for="td-search" class="sr-only">Search</label>
-                    <div class="relative w-full">
-                        <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
-                            <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
+            <div class="tdSearch-Modal" style="--tdSearch-vh:5.8px;">
+                <header class="tdSearch-SearchBar">
+                    <form class="tdSearch-Form">
+                        <label class="tdSearch-MagnifierLabel" for="tdSearch-input" id="tdSearch-label"
+                        ><svg width="20" height="20" class="tdSearch-Search-Icon" viewBox="0 0 20 20"><path d="M14.386 14.386l4.0877 4.0877-4.0877-4.0877c-2.9418 2.9419-7.7115 2.9419-10.6533 0-2.9419-2.9418-2.9419-7.7115 0-10.6533 2.9418-2.9419 7.7115-2.9419 10.6533 0 2.9419 2.9418 2.9419 7.7115 0 10.6533z" stroke="currentColor" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg
+                            ></label>
+                        <div class="tdSearch-LoadingIndicator">
+                            <svg viewBox="0 0 38 38" stroke="currentColor" stroke-opacity=".5">
+                                <g fill="none" fill-rule="evenodd">
+                                    <g transform="translate(1 1)" stroke-width="2">
+                                        <circle stroke-opacity=".3" cx="18" cy="18" r="18"></circle>
+                                        <path d="M36 18c0-9.94-8.06-18-18-18"><animateTransform attributeName="transform" type="rotate" from="0 18 18" to="360 18 18" dur="1s" repeatCount="indefinite"></animateTransform></path>
+                                    </g>
+                                </g>
+                            </svg>
                         </div>
-                        <input type="text" id="td-search" class="bg-gray-50 border border-gray-300 text-gray-900
-                        text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search" required="">
+                        <input class="tdSearch-Input" aria-autocomplete="both" aria-labelledby="tdSearch-label" id="tdSearch-input" autocomplete="off" autocorrect="off" autocapitalize="off" enterkeyhint="go" spellcheck="false" placeholder="Search documentation" maxlength="64" type="search" value="" />
+                    </form>
+                </header>
+                <div class="tdSearch-Dropdown">
+                    <div class="tdSearch-Dropdown-Container">
+                        <section class="tdSearch-Hits">
+                            <ul role="listbox" aria-labelledby="tdSearch-label" id="tdSearch-list">
+                                <li class="tdSearch-Hit" id="tdSearch-item-9" role="option" aria-selected="false">
+                                    <a class="tdSearch-Hit--Result" href="/docs/sepia#adding-sepia-filters"
+                                    ><div class="tdSearch-Hit-Container">
+                                            <div class="tdSearch-Hit-icon">
+                                                <svg width="20" height="20" viewBox="0 0 20 20"><path d="M13 13h4-4V8H7v5h6v4-4H7V8H3h4V3v5h6V3v5h4-4v5zm-6 0v4-4H3h4z" stroke="currentColor" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>
+                                            </div>
+                                            <div class="tdSearch-Hit-content-wrapper">
+                                                <span class="tdSearch-Hit-title">Ticket body</span>
+                                                <span
+                                                        class="tdSearch-Hit-path">Sepia</span>
+                                            </div>
+                                            <div class="tdSearch-Hit-action">
+                                                <svg class="tdSearch-Hit-Select-Icon" width="20" height="20" viewBox="0 0 20 20">
+                                                    <g stroke="currentColor" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round">
+                                                        <path d="M18 3v4c0 2-2 4-4 4H2"></path>
+                                                        <path d="M8 17l-6-6 6-6"></path>
+                                                    </g>
+                                                </svg>
+                                            </div></div
+                                        ></a>
+                                </li>
+                                <li class="tdSearch-Hit" id="tdSearch-item-10" role="option" aria-selected="true">
+                                    <a class="tdSearch-Hit--Result tdSearch-Hit--FirstChild tdSearch-Hit--LastChild" href="/docs/sepia#removing-filters"
+                                    ><div class="tdSearch-Hit-Container">
+                                            <div class="tdSearch-Hit-icon">
+                                                <svg width="20" height="20" viewBox="0 0 20 20"><path d="M13 13h4-4V8H7v5h6v4-4H7V8H3h4V3v5h6V3v5h4-4v5zm-6 0v4-4H3h4z" stroke="currentColor" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>
+                                            </div>
+                                            <div class="tdSearch-Hit-content-wrapper">
+                                                <span class="tdSearch-Hit-title">Removing body content</span><span
+                                                        class="tdSearch-Hit-path">Sepia</span>
+                                            </div>
+                                            <div class="tdSearch-Hit-action">
+                                                <svg class="tdSearch-Hit-Select-Icon" width="20" height="20" viewBox="0 0 20 20">
+                                                    <g stroke="currentColor" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round">
+                                                        <path d="M18 3v4c0 2-2 4-4 4H2"></path>
+                                                        <path d="M8 17l-6-6 6-6"></path>
+                                                    </g>
+                                                </svg>
+                                            </div></div
+                                        ></a>
+                                </li>
+                                <li class="tdSearch-Hit" id="tdSearch-item-11" role="option" aria-selected="false">
+                                    <a class="tdSearch-Hit--Result" href="/docs/backdrop-sepia#adding-backdrop-sepia-filters"
+                                    ><div class="tdSearch-Hit-Container">
+                                            <div class="tdSearch-Hit-icon">
+                                                <svg width="20" height="20" viewBox="0 0 20 20"><path d="M13 13h4-4V8H7v5h6v4-4H7V8H3h4V3v5h6V3v5h4-4v5zm-6 0v4-4H3h4z" stroke="currentColor" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>
+                                            </div>
+                                            <div class="tdSearch-Hit-content-wrapper">
+                                                <span class="tdSearch-Hit-title"> Adding backdrop sepia
+                                                    </span>
+                                                <span class="tdSearch-Hit-path">Backdrop Sepia</span>
+                                            </div>
+                                            <div class="tdSearch-Hit-action">
+                                                <svg class="tdSearch-Hit-Select-Icon" width="20" height="20" viewBox="0 0 20 20">
+                                                    <g stroke="currentColor" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round">
+                                                        <path d="M18 3v4c0 2-2 4-4 4H2"></path>
+                                                        <path d="M8 17l-6-6 6-6"></path>
+                                                    </g>
+                                                </svg>
+                                            </div></div
+                                        ></a>
+                                </li>
+                                <li class="tdSearch-Hit" id="tdSearch-item-11" role="option" aria-selected="false">
+                                    <a class="tdSearch-Hit--Result" href="/docs/backdrop-sepia#adding-backdrop-sepia-filters"
+                                    ><div class="tdSearch-Hit-Container">
+                                            <div class="tdSearch-Hit-icon">
+                                                <svg width="20" height="20" viewBox="0 0 20 20"><path d="M13 13h4-4V8H7v5h6v4-4H7V8H3h4V3v5h6V3v5h4-4v5zm-6 0v4-4H3h4z" stroke="currentColor" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>
+                                            </div>
+                                            <div class="tdSearch-Hit-content-wrapper">
+                                                <span class="tdSearch-Hit-title"> Adding backdrop sepia
+                                                    </span>
+                                                <span class="tdSearch-Hit-path">Backdrop Sepia</span>
+                                            </div>
+                                            <div class="tdSearch-Hit-action">
+                                                <svg class="tdSearch-Hit-Select-Icon" width="20" height="20" viewBox="0 0 20 20">
+                                                    <g stroke="currentColor" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round">
+                                                        <path d="M18 3v4c0 2-2 4-4 4H2"></path>
+                                                        <path d="M8 17l-6-6 6-6"></path>
+                                                    </g>
+                                                </svg>
+                                            </div></div
+                                        ></a>
+                                </li>
+                                <li class="tdSearch-Hit" id="tdSearch-item-11" role="option" aria-selected="false">
+                                    <a class="tdSearch-Hit--Result" href="/docs/backdrop-sepia#adding-backdrop-sepia-filters"
+                                    ><div class="tdSearch-Hit-Container">
+                                            <div class="tdSearch-Hit-icon">
+                                                <svg width="20" height="20" viewBox="0 0 20 20"><path d="M13 13h4-4V8H7v5h6v4-4H7V8H3h4V3v5h6V3v5h4-4v5zm-6 0v4-4H3h4z" stroke="currentColor" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>
+                                            </div>
+                                            <div class="tdSearch-Hit-content-wrapper">
+                                                <span class="tdSearch-Hit-title"> Adding backdrop sepia
+                                                    </span>
+                                                <span class="tdSearch-Hit-path">Backdrop Sepia</span>
+                                            </div>
+                                            <div class="tdSearch-Hit-action">
+                                                <svg class="tdSearch-Hit-Select-Icon" width="20" height="20" viewBox="0 0 20 20">
+                                                    <g stroke="currentColor" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round">
+                                                        <path d="M18 3v4c0 2-2 4-4 4H2"></path>
+                                                        <path d="M8 17l-6-6 6-6"></path>
+                                                    </g>
+                                                </svg>
+                                            </div></div
+                                        ></a>
+                                </li>
+                            </ul>
+                        </section>
+                        <section class="tdSearch-HitsFooter"></section>
                     </div>
-
-                    <button type="submit" class="p-2.5 ml-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                        <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
-                        <span class="sr-only">Search</span>
-                    </button>
-                </form>
-                <div class="dropdownClass"
-                >
-                    <ul class="py-1">
-                        <li class="px-3 py-2 cursor-pointer hover:bg-gray-200"
-                        >
-                            hhhkk
-                        </li>
-                        <li class="px-3 py-2 text-center">
-                            No Matching Results
-                        </li>
-                    </ul>
                 </div>
+                <footer class="tdSearch-Footer">
+                    <div class="flex-s p-1 space-x-2">
+                        <a href="<?php echo get_page_link( get_post(getSelectedHelpdeskOptions('td_helpdesk_settings')['td_form_page_id']))?>" target="_blank" class="text-gray-600 text-sm">
+                            Create new
+                        </a>
+                    </div>
+                </footer>
+            </div>
 
-            </div>
             <!-- Modal footer -->
-            <div class="flex items-center p-6 space-x-2 rounded-b border-t border-gray-200 dark:border-gray-600">
-                <a href="<?php echo get_page_link( get_post(getSelectedHelpdeskOptions('td_helpdesk_options')['td_form_page_id']))?>" target="_blank" class="text-center text-gray-600 text-sm">
-                    Create new
-                </a>
-            </div>
+
         </div>
     </div>
 </div>
