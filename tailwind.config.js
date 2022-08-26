fbe29354acd900c6c5c8997c743bc05e2ab15692
Parvez Akther <parvez@themexpert.com>
2022-08-26 22:31:20 +0300

    included cssnano plugin and configure tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
 module.exports = {
 	content: ['./includes/**/*.{html,js,jsx,ts,tsx,vue,php}'],
+	important: true,
 	theme: {
 		extend: {},
+	},
+	corePlugins: {
+		float: false,
+		objectFit: false,
+		objectPosition: false,
 	},
 	plugins: [
 		require('@tailwindcss/typography'),
