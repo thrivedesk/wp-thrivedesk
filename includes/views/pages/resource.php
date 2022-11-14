4531334014de08a4f489cf63ba6b3d08347b445d
atiq-ur <ar.diu.cse@gmail.com>
2022-11-14 14:26:08 +0600

    fix: nested tab issue

--- includes/views/pages/resource.php
+++ includes/views/pages/resource.php
@@ -9,7 +9,7 @@
             <a data-target="tab-resource-themes" href="#resource-themes"><?php _e('Themes', 'thrivedesk');?></a>
         </li>
         <li>
-            <a href="#"><?php _e('Services', 'thrivedesk');?></a>
+            <a data-target="tab-resource-services" href="#resource-services"><?php _e('Services', 'thrivedesk');?></a>
         </li>
     </ul>
 
@@ -26,6 +26,14 @@
             <a class="px-6 py-10 bg-white space-y-4 rounded shadow hover:shadow-lg hover:scale-105 transition ease-linear delay-300 " href="https://www.thrivedesk.com/?utm_source=thrivedesk_plugin&utm_medium=link&utm_campaign=resources">
                 <img class="mb-10" src="<?php echo THRIVEDESK_PLUGIN_ASSETS . '/images/thriveDesk.png'; ?>" alt="ThriveDesk">
                 <h3 class="text-xl font-bold">ThriveDesk Themes</h3>
+                <p class="text-base">Scale your business with WordPress on the front-end and allow BigCommerce to free up server resources on the back-end.</p>
+            </a>
+        </div>
+
+        <div class="hidden tab-resource-services grid grid-cols-3 gap-5">
+            <a class="px-6 py-10 bg-white space-y-4 rounded shadow hover:shadow-lg hover:scale-105 transition ease-linear delay-300 " href="https://www.thrivedesk.com/?utm_source=thrivedesk_plugin&utm_medium=link&utm_campaign=resources">
+                <img class="mb-10" src="<?php echo THRIVEDESK_PLUGIN_ASSETS . '/images/thriveDesk.png'; ?>" alt="ThriveDesk">
+                <h3 class="text-xl font-bold">ThriveDesk Service</h3>
                 <p class="text-base">Scale your business with WordPress on the front-end and allow BigCommerce to free up server resources on the back-end.</p>
             </a>
         </div>
