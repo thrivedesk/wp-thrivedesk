0009a9d7779db865f0766eaba5064da67497f462
Md Atiqur Rahman <ar.diu.cse@gmail.com>
2022-12-19 14:18:27 +0600

    add: service class for api requests

---
+++ src/Services/TDApiService.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace ThriveDesk\Services;
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+class TDApiService {
+    private string $api_token;
+    private string $org_slug;
+
+    public function __construct()
+    {
+        $this->api_token = get_option('td_helpdesk_settings')['td_helpdesk_api_key'];
+        $this->org_slug = get_option('td_helpdesk_settings')['td_helpdesk_organization_slug'];
+    }
+
+    public function postRequest(string $url, array $data = []){
+
+        $args     = [
+            'headers' => [
+                'Authorization' => 'Bearer ' . $this->api_token,
+                'X-Td-Organization-Slug' => $this->org_slug,
+            ],
+            'body'    => $data,
+        ];
+
+        $response           = wp_remote_post($url, $args);
+        $body               = wp_remote_retrieve_body($response);
+        return json_decode($body, true);
+    }
+
+    public function getRequest(string $url)
+    {
+        $args               = [
+            'headers' => [
+                'Authorization' => 'Bearer ' . $this->api_token,
+                'Content-Type'  => 'application/json',
+                'Accept'        => 'application/json',
+                'X-Td-Organization-Slug' => $this->org_slug,
+            ],
+        ];
+
+        $response           = wp_remote_get($url, $args);
+        $body               = wp_remote_retrieve_body($response);
+        $body               = json_decode($body, true);
+        return $body ?? [];
+    }
+}
