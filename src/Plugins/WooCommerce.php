872551bb3ad67585106ce1eabdd3d6868f31f871
Rijoanul-Shanto <rijoanul.shanto@gmail.com>
2022-12-25 17:21:14 +0600

    fix: review update

--- src/Plugins/WooCommerce.php
+++ src/Plugins/WooCommerce.php
@@ -64,7 +64,9 @@ final class WooCommerce extends Plugin
      */
     public static function is_plugin_active(): bool
     {
-        if (!function_exists('WC') || !class_exists('WooCommerce', false)) return false;
+        if (!function_exists('WC') || !class_exists('WooCommerce', false)) {
+            return false;
+        }
 
         return true;
     }
@@ -79,7 +81,10 @@ final class WooCommerce extends Plugin
         if (empty($this->orders)) {
             $this->orders = $this->get_orders();
         }
-        if (!empty($this->orders)) return true;
+        if (!empty($this->orders)) {
+            return true;
+        }
+
         return false;
     }
 
@@ -90,14 +95,18 @@ final class WooCommerce extends Plugin
      */
     public function is_customer_exist(): bool
     {
-        if (!$this->customer_email) return false;
+        if (!$this->customer_email) {
+            return false;
+        }
 
         if (!$this->customer) {
-            $user_id = get_user_by('email', $this->customer_email)->ID ?? 0;
+            $user_id        = get_user_by('email', $this->customer_email)->ID ?? 0;
             $this->customer = new \WC_Customer($user_id);
         }
 
-        if (!$this->customer->get_id() && !$this->is_guest()) return false;
+        if (!$this->customer->get_id() && !$this->is_guest()) {
+            return false;
+        }
 
         return true;
     }
@@ -119,25 +128,30 @@ final class WooCommerce extends Plugin
      */
     public function get_customer(): array
     {
-        if (!$this->customer_email) return [];
+        if (!$this->customer_email) {
+            return [];
+        }
 
         if (!$this->customer) {
-            $user_id = get_user_by('email', $this->customer_email)->ID ?? 0;
+            $user_id        = get_user_by('email', $this->customer_email)->ID ?? 0;
             $this->customer = new \WC_Customer($user_id);
         }
 
-        if (!$this->customer->get_id()) return [];
+        if (!$this->customer->get_id()) {
+            return [];
+        }
 
         return [
-            'name' => $this->customer->get_display_name() ?? '',
-            'registered_at' => date('d M Y', strtotime($this->customer->get_date_created())) ?? ''
+            'name'          => $this->customer->get_display_name() ?? '',
+            'registered_at' => date('d M Y', strtotime($this->customer->get_date_created())) ?? '',
         ];
     }
 
     /**
-     * Get the formated amount
+     * Get the formatted amount
      *
-     * @param float $amount
+     * @param  float  $amount
+     *
      * @return string
      */
     public function get_formated_amount(float $amount): string
@@ -153,41 +167,23 @@ final class WooCommerce extends Plugin
      */
     public function get_orders(): array
     {
-
         if (empty($this->orders) && !$this->isCalled) {
             $query = new WC_Order_Query();
             $query->set('customer', $this->customer_email);
             $customer_orders = $query->get_orders();
             $this->isCalled  = true;
 
-            if ($this->shipping_param) {
-                foreach ($customer_orders as $order) {
-                    array_push($this->orders, [
-                        'order_id'        => $order->get_id(),
-                        'amount'          => (float)$order->get_total(),
-                        'amount_formated' => $this->get_formated_amount($order->get_total()),
-                        'date'            => date('d M Y', strtotime($order->get_date_created())),
-                        'order_status'    => ucfirst($order->get_status()),
-                        'shipping'        => $this->get_shipping_details($order),
-                        'downloads'       => $this->get_order_items($order),
-                        'order_url'       => method_exists($order,
-                            'get_edit_order_url') ? $order->get_edit_order_url() : '#',
-                    ]);
-                }
-            } else {
-                foreach ($customer_orders as $order) {
-                    array_push($this->orders, [
-                        'order_id'        => $order->get_id(),
-                        'amount'          => (float)$order->get_total(),
-                        'amount_formated' => $this->get_formated_amount($order->get_total()),
-                        'date'            => date('d M Y', strtotime($order->get_date_created())),
-                        'order_status'    => ucfirst($order->get_status()),
-                        'shipping'        => [],
-                        'downloads'       => $this->get_order_items($order),
-                        'order_url'       => method_exists($order,
-                            'get_edit_order_url') ? $order->get_edit_order_url() : '#',
-                    ]);
-                }
+            foreach ($customer_orders as $order) {
+                array_push($this->orders, [
+                    'order_id'        => $order->get_id(),
+                    'amount'          => (float)$order->get_total(),
+                    'amount_formated' => $this->get_formated_amount($order->get_total()),
+                    'date'            => date('d M Y', strtotime($order->get_date_created())),
+                    'order_status'    => ucfirst($order->get_status()),
+                    'shipping'        => $this->shipping_param ? $this->get_shipping_details($order) : [],
+                    'downloads'       => $this->get_order_items($order),
+                    'order_url'       => method_exists($order, 'get_edit_order_url') ? $order->get_edit_order_url() : '#',
+                ]);
             }
         }
 
@@ -198,26 +194,31 @@ final class WooCommerce extends Plugin
      * get woocommerce order status
      *
      * @param $order_id
+     *
      * @return array
      *
      * @since 0.8.4
      */
     public function order_status($order_id): array
     {
-        if (!$this->is_customer_exist()) return [];
+        if (!$this->is_customer_exist()) {
+            return [];
+        }
 
         $order = wc_get_order($order_id);
 
-        if (!$order) return [];
+        if (!$order) {
+            return [];
+        }
 
         return [
-            'order_id' => $order->get_id(),
-            'amount' => $order->get_total(),
+            'order_id'         => $order->get_id(),
+            'amount'           => $order->get_total(),
             'amount_formatted' => $this->get_formated_amount($order->get_total()),
-            'date' => date('d M Y', strtotime($order->get_date_created())),
-            'order_status' => ucfirst($order->get_status()),
-            'shipping' => $this->get_shipping_details($order),
-            'downloads' => $this->get_order_items($order),
+            'date'             => date('d M Y', strtotime($order->get_date_created())),
+            'order_status'     => ucfirst($order->get_status()),
+            'shipping'         => $this->get_shipping_details($order),
+            'downloads'        => $this->get_order_items($order),
         ];
     }
 
@@ -226,22 +227,24 @@ final class WooCommerce extends Plugin
      * get order shipping details
      *
      * @param $order
+     *
      * @return array
      */
     public function get_shipping_details($order): array
     {
         $states = WC()->countries->get_states($order->get_shipping_country());
-        $state = !empty($states[$order->get_shipping_state()]) ? $states[$order->get_shipping_state()] : '';
+        $state  = !empty($states[$order->get_shipping_state()]) ? $states[$order->get_shipping_state()] : '';
 
         $shipping_details = [];
 
         array_push($shipping_details, [
-            'street' => $order->get_shipping_address_1() ?? '',
-            'city' => $order->get_shipping_city() ?? '',
-            'zip' => $order->get_shipping_postcode() ?? '',
-            'state' => $state,
+            'street'  => $order->get_shipping_address_1() ?? '',
+            'city'    => $order->get_shipping_city() ?? '',
+            'zip'     => $order->get_shipping_postcode() ?? '',
+            'state'   => $state,
             'country' => WC()->countries->countries[$order->get_shipping_country()] ?? '',
         ]);
+
         return $shipping_details;
     }
 
@@ -249,18 +252,20 @@ final class WooCommerce extends Plugin
      * check if site url starts with http:// or https://
      *
      * @param $site_url
+     *
      * @return bool
      */
     public function check_site_url($site_url): bool
     {
         return substr($site_url, 0, 7) === "http://" ||
-            substr($site_url, 0, 8) === "https://";
+               substr($site_url, 0, 8) === "https://";
     }
 
     /**
      * get order items license details
      *
      * @param $order
+     *
      * @return array
      */
     public function get_order_items($order): array
@@ -268,7 +273,7 @@ final class WooCommerce extends Plugin
         $items = $order->get_items();
 
         $download_item = [];
-        $license_info = [];
+        $license_info  = [];
 
         if (method_exists('WOO_SL_functions', 'get_order_licence_details')) {
 
@@ -291,7 +296,8 @@ final class WooCommerce extends Plugin
 
                     $sites = [];
 
-                    $expire_date = intval(\WOO_SL_functions::get_order_item_meta($orderLicense->order_item_id, '_woo_sl_licensing_expire_at') ?? '');
+                    $expire_date = intval(\WOO_SL_functions::get_order_item_meta($orderLicense->order_item_id,
+                        '_woo_sl_licensing_expire_at') ?? '');
                     $expire_date = $expire_date == 0 ? '' : date("d M Y", $expire_date);
 
                     $woo_site_url = '';
@@ -306,27 +312,27 @@ final class WooCommerce extends Plugin
                     }
 
                     $license_info[$license->order_item_id] = [
-                        'key' => $license->licence ?? '',
+                        'key'              => $license->licence ?? '',
                         'activation_limit' => $orderLicense->license_data["max_instances_per_key"],
-                        'sites' => $sites,
-                        'date_created' => $license->created ?? '',
-                        'expiration' => $expire_date,
-                        'is_lifetime' => $orderLicense->license_data['product_use_expire'] == 'no',
-                        'status' => \WOO_SL_functions::get_licence_key_status($license->id) ?? '',
+                        'sites'            => $sites,
+                        'date_created'     => $license->created ?? '',
+                        'expiration'       => $expire_date,
+                        'is_lifetime'      => $orderLicense->license_data['product_use_expire'] == 'no',
+                        'status'           => \WOO_SL_functions::get_licence_key_status($license->id) ?? '',
                     ];
                 }
             }
         }
         foreach ($items as $item) {
             if (array_key_exists($item->get_id(), $license_info)) {
-                array_push($download_item, array(
-                    "title" => $item["name"],
+                array_push($download_item, [
+                    "title"   => $item["name"],
                     "license" => $license_info[$item->get_id()],
-                ));
+                ]);
             } else {
-                array_push($download_item, array(
+                array_push($download_item, [
                     "title" => $item["name"],
-                ));
+                ]);
             }
         }
 
@@ -344,7 +350,7 @@ final class WooCommerce extends Plugin
 
     public function connect()
     {
-        $thrivedesk_options = get_option('thrivedesk_options', []);
+        $thrivedesk_options                = get_option('thrivedesk_options', []);
         $thrivedesk_options['woocommerce'] = $thrivedesk_options['woocommerce'] ?? [];
 
         $thrivedesk_options['woocommerce']['connected'] = true;
@@ -354,7 +360,7 @@ final class WooCommerce extends Plugin
 
     public function disconnect()
     {
-        $thrivedesk_options = get_option('thrivedesk_options', []);
+        $thrivedesk_options                = get_option('thrivedesk_options', []);
         $thrivedesk_options['woocommerce'] = $thrivedesk_options['woocommerce'] ?? [];
 
         $thrivedesk_options['woocommerce'] = [
