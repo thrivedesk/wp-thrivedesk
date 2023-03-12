ee739663fb0b24bb5c050563c70125eaacf76143
Parvez Akther <parvez@themexpert.com>
2023-03-12 23:34:13 +0600

    package update

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -23,15 +23,38 @@ class ComposerAutoloaderInit61ac37121bc927c3837fa0a0819ce1b8
         }
 
         spl_autoload_register(array('ComposerAutoloaderInit61ac37121bc927c3837fa0a0819ce1b8', 'loadClassLoader'), true, true);
-        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
+        self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInit61ac37121bc927c3837fa0a0819ce1b8', 'loadClassLoader'));
 
-        require __DIR__ . '/autoload_static.php';
-        call_user_func(\Composer\Autoload\ComposerStaticInit61ac37121bc927c3837fa0a0819ce1b8::getInitializer($loader));
+        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
+        if ($useStaticLoader) {
+            require __DIR__ . '/autoload_static.php';
+
+            call_user_func(\Composer\Autoload\ComposerStaticInit61ac37121bc927c3837fa0a0819ce1b8::getInitializer($loader));
+        } else {
+            $map = require __DIR__ . '/autoload_namespaces.php';
+            foreach ($map as $namespace => $path) {
+                $loader->set($namespace, $path);
+            }
+
+            $map = require __DIR__ . '/autoload_psr4.php';
+            foreach ($map as $namespace => $path) {
+                $loader->setPsr4($namespace, $path);
+            }
+
+            $classMap = require __DIR__ . '/autoload_classmap.php';
+            if ($classMap) {
+                $loader->addClassMap($classMap);
+            }
+        }
 
         $loader->register(true);
 
-        $includeFiles = \Composer\Autoload\ComposerStaticInit61ac37121bc927c3837fa0a0819ce1b8::$files;
+        if ($useStaticLoader) {
+            $includeFiles = Composer\Autoload\ComposerStaticInit61ac37121bc927c3837fa0a0819ce1b8::$files;
+        } else {
+            $includeFiles = require __DIR__ . '/autoload_files.php';
+        }
         foreach ($includeFiles as $fileIdentifier => $file) {
             composerRequire61ac37121bc927c3837fa0a0819ce1b8($fileIdentifier, $file);
         }
@@ -40,16 +63,11 @@ class ComposerAutoloaderInit61ac37121bc927c3837fa0a0819ce1b8
     }
 }
 
-/**
- * @param string $fileIdentifier
- * @param string $file
- * @return void
- */
 function composerRequire61ac37121bc927c3837fa0a0819ce1b8($fileIdentifier, $file)
 {
     if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
-        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
-
         require $file;
+
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,23 +1,24 @@
-<?php return array(
-    'root' => array(
-        'name' => 'thrivedesk/wp-plugin',
-        'pretty_version' => 'dev-develop',
-        'version' => 'dev-develop',
-        'reference' => '5a9a7a02f889213205b06bea1c23c99569881912',
-        'type' => 'library',
-        'install_path' => __DIR__ . '/../../',
-        'aliases' => array(),
-        'dev' => true,
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-develop',
+    'version' => 'dev-develop',
+    'aliases' => 
+    array (
     ),
-    'versions' => array(
-        'thrivedesk/wp-plugin' => array(
-            'pretty_version' => 'dev-develop',
-            'version' => 'dev-develop',
-            'reference' => '5a9a7a02f889213205b06bea1c23c99569881912',
-            'type' => 'library',
-            'install_path' => __DIR__ . '/../../',
-            'aliases' => array(),
-            'dev_requirement' => false,
-        ),
+    'reference' => '23f544a7f9a34a1e77b6f8ed9dbbc51f2033480b',
+    'name' => 'thrivedesk/wp-plugin',
+  ),
+  'versions' => 
+  array (
+    'thrivedesk/wp-plugin' => 
+    array (
+      'pretty_version' => 'dev-develop',
+      'version' => 'dev-develop',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '23f544a7f9a34a1e77b6f8ed9dbbc51f2033480b',
     ),
+  ),
 );
