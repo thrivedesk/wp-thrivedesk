ad57fe8b3e14e98f7563bcad96f6cd08a2d38b9c
Parvez Akther <parvez@themexpert.com>
2024-01-03 12:44:17 +0300

    conversation page style done

--- includes/views/shortcode/conversation-details.php
+++ includes/views/shortcode/conversation-details.php
@@ -16,32 +16,28 @@ if (isset($query_params['td_conversation_id'])) {
 }
 ?>
 <?php if ($is_portal_available && $conversation): ?>
-<div class="td-portal-conversations">
+<div id="thrivedesk" class="td-portal-conversations space-y-4">
+    
+    <a href="<?php echo get_permalink(); ?>" class="border rounded-full px-2.5 py-1 bg-white hover:bg-slate-50">
+        <span>←</span>
+        <span><?php _e('Back to tickets', 'thrivedesk'); ?></span>
+    </a>
     <!-- header  -->
-    <div class="flex">
-        <a href="<?php echo get_permalink(); ?>" class="text-slate-500 flex items-center mb-4 space-x-0
-        hover:text-slate-700">
-            <?php thrivedesk_view('icons/back'); ?>
-            <span><?php _e('Back to tickets', 'thrivedesk'); ?></span>
-        </a>
-    </div>
-
     <div class="flex items-center">
-       <div class="flex-auto">
-        <span>#<?php echo $conversation['ticket_id'];?></span>
-            <h2 class="text-2xl font-bold mt-0 mb-1 text-black"><?php echo $conversation['subject']?></h2>
-            <span class="text-sm">
-                <?php _e('Last update', 'thrivedesk'); ?>:
-                <?php echo diff_for_humans($conversation['updated_at']) ?>
-            </span>
-       </div>
-       <span class="status status-<?php echo strtolower($conversation['status']); ?>"><?php echo $conversation['status']; ?></span>
+        <div class="flex-auto">
+            <div class="flex space-x-1 text-slate-500">
+                <span>[#<?php echo $conversation['ticket_id'];?>]</span>
+                <span><?php echo diff_for_humans($conversation['updated_at']) ?></span>
+            </div>
+            <h1 class="text-2xl font-bold mt-0 mb-1 text-black"><?php echo $conversation['subject']?></h1>
+        </div>
+        <span class="status status-<?php echo strtolower($conversation['status']); ?>"><?php echo $conversation['status']; ?></span>
     </div>
     <!-- conversations  -->
-    <div class="mt-5 space-y-6">
+    <div class="space-y-4">
         <?php foreach ($conversation['events'] as $event): ?>
 	        <?php if ($event['event']): ?>
-                <?php $actor_name = $actor_name ?? ''; ?>
+                <?php $actor_name = $event['actor']['name'] ?? ''; ?>
                 <div class="td-conversation <?php echo $event['actor_type'] == ACTOR_TYPE ? 'actor-contact' : 'actor-agent';?>">
                     <div class="td-conversation-header">
                         <div class="flex items-center space-x-2 flex-auto">
@@ -52,9 +48,9 @@ if (isset($query_params['td_conversation_id'])) {
                             <span class="font-bold"><?php echo $actor_name; ?></span>
                             <span><?php echo $event['action'];?></span>
                         </div>
-                        <span class="text-sm"><?php echo diff_for_humans($event['created_at']); ?></span>
+                        <span class="text-sm ml-auto text-slate-800/50"><?php echo diff_for_humans($event['created_at']); ?></span>
                     </div>
-                    <div class="td-conversation-body" dir="auto">
+                    <div class="td-conversation-body py-4" dir="auto">
 				        <?php if ($event['event']['html_body']): ?>
 					        <?php echo Conversation::validate_conversation_body($event['event']['html_body']); ?>
 				        <?php elseif($event['event']['text_body']): ?>
@@ -66,7 +62,7 @@ if (isset($query_params['td_conversation_id'])) {
         <?php endforeach; ?>
 
         <!-- Reply editor -->
-        <div class="mt-10 pt-10 border-t">
+        <div>
             <form action="" id="td_conversation_reply" method="POST">
                 <input type="hidden" id="td_reply_none" value="<?php echo $td_reply_nonce; ?>">
                 
@@ -80,7 +76,7 @@ if (isset($query_params['td_conversation_id'])) {
                         class="td-btn-primary px-8 mt-6">
 		            <?php _e('Reply', 'thrivedesk'); ?>
                     <span id="td-reply-spinner" style="display: none;">
-                            <?php thrivedesk_view('/icons/spinner'); ?>
+                        <?php thrivedesk_view('/icons/spinner'); ?>
                     </span>
                 </button>
             </form>
@@ -89,7 +85,7 @@ if (isset($query_params['td_conversation_id'])) {
 </div>
 
 <?php else: ?>
-    <div class="p-10 text-center">
-        <span><?php _e('Portal feature is not available at this moment.', 'thrivedesk'); ?></span>
+    <div class="p-10 text-center my-10 bg-rose-50 border-2 border-dashed border-rose-200 text-rose-500 rounded font-medium">
+        <span><?php _e('Your subscription plan does not support WPPortal feature. Please contact ThriveDesk for more information.', 'thrivedesk'); ?></span>
     </div>
 <?php endif; ?>
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,23 +1,24 @@
-<?php return array(
-    'root' => array(
-        'name' => 'thrivedesk/wp-plugin',
-        'pretty_version' => 'dev-develop',
-        'version' => 'dev-develop',
-        'reference' => 'ddce76edacf71ece5ba0f7cd9268ff07fb33f140',
-        'type' => 'library',
-        'install_path' => __DIR__ . '/../../',
-        'aliases' => array(),
-        'dev' => false,
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-develop',
+    'version' => 'dev-develop',
+    'aliases' => 
+    array (
     ),
-    'versions' => array(
-        'thrivedesk/wp-plugin' => array(
-            'pretty_version' => 'dev-develop',
-            'version' => 'dev-develop',
-            'reference' => 'ddce76edacf71ece5ba0f7cd9268ff07fb33f140',
-            'type' => 'library',
-            'install_path' => __DIR__ . '/../../',
-            'aliases' => array(),
-            'dev_requirement' => false,
-        ),
+    'reference' => '8262b812b8c50bb405ff2ccf812c2326ac03b1ae',
+    'name' => 'thrivedesk/wp-plugin',
+  ),
+  'versions' => 
+  array (
+    'thrivedesk/wp-plugin' => 
+    array (
+      'pretty_version' => 'dev-develop',
+      'version' => 'dev-develop',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '8262b812b8c50bb405ff2ccf812c2326ac03b1ae',
     ),
+  ),
 );
