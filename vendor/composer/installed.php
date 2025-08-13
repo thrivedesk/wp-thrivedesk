d7b127653ff187268dbb9400924929ca5881249d
Abu Huraira Bin Aman <shams.bd71@gmail.com>
2025-08-13 16:52:10 +0600

    composer added to release files

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'thrivedesk/wp-plugin',
         'pretty_version' => 'dev-develop',
         'version' => 'dev-develop',
-        'reference' => '4ed41c66226d289979abe71e8532121f1566f788',
+        'reference' => '29ce53db4fa39037feddc576f6d3c2c01c75eac2',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         'thrivedesk/wp-plugin' => array(
             'pretty_version' => 'dev-develop',
             'version' => 'dev-develop',
-            'reference' => '4ed41c66226d289979abe71e8532121f1566f788',
+            'reference' => '29ce53db4fa39037feddc576f6d3c2c01c75eac2',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
