5997decc29231efdc9bf06f35035ab17dd8e97b6
Abu Huraira Bin Aman <shams.bd71@gmail.com>
2025-08-28 15:01:54 +0600

    2.1.3 - release and changelog

--- thrivedesk.php
+++ thrivedesk.php
@@ -5,7 +5,7 @@
  * Description:         Live Chat, Help Desk & Knowledge Base plugin for WordPress
  * Plugin URI:          https://www.thrivedesk.com/?utm_source=wp-plugins&utm_campaign=plugin-uri&utm_medium=wp-dash
  * Tags:                live chat, helpdesk, free live chat, knowledge base, thrivedesk
- * Version:             2.1.2
+ * Version:             2.1.3
  * Author:              ThriveDesk
  * Author URI:          https://profiles.wordpress.org/thrivedesk/
  * Text Domain:         thrivedesk
@@ -54,7 +54,7 @@ final class ThriveDesk
      *
      * @var string
      */
-    public $version = '2.1.2';
+    public $version = '2.1.3';
 
     /**
      * The single instance of this class
